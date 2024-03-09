684fe0416d83b132d5ebab4c2f4c19372895fd91
waseemuddin <waseem.uddin@live.com>
2024-03-09 11:29:19 +0500

    java code comipled and run

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ public class helloworld {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello World - This is java first program");
+        System.out.print("Hello World - This is java first program\n -  \n This is java first program\n");
     }
 }
