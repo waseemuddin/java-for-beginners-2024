90b16cc21a83f9a323ea17a37def4f8141686bb3
ShaikhWaseem <waseem.uddin@live.com>
2024-03-25 23:58:02 +0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ public class helloworld {
 
     public static void main(String[] args) {
         
-        System.out.print("Hello World 22- This is java first program\n -  \n This is java first program\n");
+        System.out.print("Hello World- This is java first program\n -  \n This is java first program\n");
     }
 }
